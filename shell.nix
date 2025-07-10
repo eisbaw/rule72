bfd2a81921cb88522e37e13157a11b9ffe0e1fea
Mark Ruvald Pedersen <wabsie@gmail.com>
2025-07-10 23:37:09 +0200

    nix: update shell.nix to default to nixpkgs 25.05
    - impure so crates can be fetched
    - fetchTarball used to pin nixpkgs to version 25.05
    - ensures consistent environment across builds
    - avoids potential issues with upstream changes in nixpkgs
    - no breaking changes introduced

--- shell.nix
+++ shell.nix
@@ -1,4 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? (import (builtins.fetchTarball {
+  url = "https://github.com/nixos/nixpkgs/tarball/25.05";
+  sha256 = "1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
+}) {}) }:
 
 pkgs.mkShell {
   buildInputs = [
